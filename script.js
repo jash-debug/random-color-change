b7226c77272015347ec2970fecdb461da0523002
ahmed najash <64089437+jash-debug@users.noreply.github.com>
2022-04-15 07:49:01 +0000

    transition

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let x=document.querySelector(".column1");
+let a=document.querySelector(".column2");
+let b=document.querySelector(".column3");
 
 let y= ()=>{
   let z=Math.floor(Math.random() * 256);
   return z;
 }
 console.log(y());
+console.log(y());
+console.log(y());
+
 x.onclick = ()=>{
   x.style.backgroundColor = `rgb(${y()},${y()},${y()})`;
+}
+
+a.onclick = ()=>{
+  a.style.backgroundColor = `rgb(${y()},${y()},${y()})`;
+}
+
+b.onclick = ()=>{
+  b.style.backgroundColor = `rgb(${y()},${y()},${y()})`;
 }
